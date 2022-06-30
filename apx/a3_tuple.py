748851a95d6a2c79e731ef3fcb79588fc7969ab2
mjh117 <jhmun117@gmail.com>
2022-07-01 08:44:54 +0900

    Remove comment in a3_tuple.py

--- apx/a3_tuple.py
+++ apx/a3_tuple.py
@@ -8,7 +8,7 @@ print( a_tuple[2] ) #결과→ 2
 print( len(a_tuple) ) #결과→ 3
 
 # 2차원 튜플
-b_tuple = ((7, 8), a_tuple) #결과→ (1, 2, (0, 1, 2))
+b_tuple = ((7, 8), a_tuple)
 print(b_tuple[1][0]) #결과→ 0
 
 # 튜플은 변경할 수 없다
